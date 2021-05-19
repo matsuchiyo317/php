4782521da8c5afc57f528284ab65ba88692f8fbd
mutomasato <matsuchiyo317@gmail.com>
2021-05-19 18:18:01 +0900

    laravel-pj

--- php-test/index.php
+++ php-test/index.php
@@ -1,2 +1,39 @@
 <?php
-echo 'Hello';
+$Company = "estra";
+$Service = "COACHTECH";
+
+echo $Company . "は" . $service . "を運営しています";
+
+echo "\n";
+
+for ($i = 1; $i <= 100; $i++) {
+  if ($i % 3 === 0 && $i % 5 === 0) {
+    echo "fizzbuzz";
+    echo "\n";
+    continue;
+  }
+  elseif ($i % 3 == 0) {
+    echo "fizz";
+    echo "\n";
+    continue;
+  }
+  elseif ($i % 5 == 0) {
+    echo "buzz";
+    echo "\n";
+    continue;
+  }
+  else {
+    echo $i;
+    echo "\n";
+  }
+}
+
+function getSquareArea($base, $height) {
+  return $base * $height;
+}
+function getTriangleArea($base, $height) {
+  return $base * $height / 2;
+}
+function getTrapezoidArea($upperBase, $lowerBase, $height) {
+  return ($upperBase + $lowerBase) * $height / 2;
+}
