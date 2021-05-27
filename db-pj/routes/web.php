56fbd665e0323290bc946ce380d73fc8e206f4eb
mutomasato <matsuchiyo317@gmail.com>
2021-05-27 17:39:14 +0900

    php追加

---
+++ db-pj/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DbclassController;
+
+Route::get('/', [DbclassController::class, 'index']);
+Route::get('/add', [DbclassController::class, 'add']);
+Route::post('/add', [DbclassController::class, 'create']);
+Route::get('/edit', [DbclassController::class, 'edit']);
+Route::post('/edit', [DbclassController::class, 'update']);
+Route::get('/delete', [DbclassController::class, 'delete']);
+Route::post('/delete', [DbclassController::class, 'remove']);
